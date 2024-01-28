69eaf73e96b1284c470c98a8d9b55169a3d1248e
kyung-yeon <kissob4905@gmail.com>
2024-01-28 15:16:34 +0900

    router 분리

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express')
+const bodyParser = require('body-parser');
+
+const userRouter = require('./routers/user.router');
+const resumeRouter = require('./routers/resume.router');
+
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+app.use(bodyParser.json());
+
+app.use('/users', userRouter);
+app.use('/resumes', resumeRouter);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
