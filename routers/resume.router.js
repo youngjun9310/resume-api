71d0d9b834b5cd521672aa1a4e783699788c4b19
kyung-yeon <kissob4905@gmail.com>
2024-01-28 15:57:12 +0900

    이력서 목록 조회 api

--- routers/resume.router.js
+++ routers/resume.router.js
@@ -1,8 +1,48 @@
 const express = require('express');
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+const jwtwebToken = require('jsonwebtoken');
+const jwtValidate = require('../middleware/jwt-validate.middleware');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('resume router');
+router.get('/', async (req, res) => {
+    const orderKey = req.query.orderKey ?? 'resumeId';
+    const orderValue = req.query.orderValue ?? 'desc';
+
+    if (!['resumeId', 'status'].includes(orderKey)) {
+        return res.status(400).json({
+            success: false,
+            message: 'orderKey 가 올바르지 않습니다.'
+        })
+    }
+
+    if (!['asc', 'desc'].includes(orderValue.toLowerCase())) {
+        return res.status(400).json({
+            success: false,
+            message: 'orderValue 가 올바르지 않습니다.'
+        })
+    }
+
+    const resumes = await prisma.resume.findMany({
+        select: {
+            resumeId: true,
+            title: true,
+            content: true,
+            user: {
+                select: {
+                    name: true,
+                }
+            },
+            createdAt: true,
+        },
+        orderBy: [
+            {
+                [orderKey]: orderValue,
+            }
+        ]
+    })
+
+    return res.json({ data: resumes });
 })
 
 module.exports = router;
