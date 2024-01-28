d898c9e44a586372f9a49229deae9f00a00fe3de
kyung-yeon <kissob4905@gmail.com>
2024-01-28 16:18:32 +0900

    이력서 수정 api

--- routers/resume.router.js
+++ routers/resume.router.js
@@ -106,7 +106,83 @@ router.post('/', jwtValidate, async (req, res) => {
         }
     })
 
-    return res.status(201).json({});
+    return res.status(201).end();
+})
+
+router.patch('/:resumeId', jwtValidate, async (req, res) => {
+    const user = res.locals.user;
+    const resumeId = req.params.resumeId;
+    const { title, content, status } = req.body;
+
+    if (!resumeId) {
+        return res.status(400).json({
+            success: false,
+            message: 'resumeId 는 필수값입니다',
+        })
+    }
+
+    if (!title) {
+        return res.status(400).json({
+            success: false,
+            message: '이력서 제목은 필수값입니다',
+        })
+    }
+
+    if (!content) {
+        return res.status(400).json({
+            success: false,
+            message: '자기소개는 필수값입니다',
+        })
+    }
+
+    if (!status) {
+        return res.status(400).json({
+            success: false,
+            message: '상태값은 필수값입니다',
+        })
+    }
+
+    // status 는 존재
+    if (!['APPLY', 'DROP', 'PASS', 'INTERVIEW1', 'INTERVIEW2', 'FINAL_PASS'].includes(status)) {
+        return res.status(400).json({
+            success: false,
+            message: '올바르지 않은 상태값 입니다.',
+        })
+    }
+
+    const resume = await prisma.resume.findFirst({
+        where: {
+            resumeId: Number(resumeId),
+        }
+    });
+
+    if (!resume) {
+        return res.status(400).json({
+            success: false,
+            message: '존재하지 않는 이력서 입니다.',
+        })
+    }
+
+    if (resume.userId !== user.userId) {
+        return res.status(400).json({
+            success: false,
+            message: '올바르지 않은 요청입니다.',
+        })
+    }
+
+    // 내가 작성한 이력서이다.
+    await prisma.resume.update({
+        where: {
+            resumeId: Number(resumeId),
+        },
+        data: {
+            title,
+            content,
+            status
+        }
+    })
+
+    return res.status(201).end()
 })
 
 module.exports = router;
